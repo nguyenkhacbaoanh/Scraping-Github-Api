89668817c13f4ecbc7e590a7e7b8d7c62fa05d39
nguyen khac bao anh <khacbaoanh.nguyen@gmail.com>
2019-01-24 19:26:01 +0100

    changement of bio infomation with multiple acc

--- scrapping.py
+++ scrapping.py
@@ -22,13 +22,24 @@ per_session = session.post(url_login,
 key_search = ["repositories", "stars", "followers", "following"]
 try:
    # information hors de boucle
-   # soup = BeautifulSoup(session.get("https://github.com/supig").content, 'html.parser')
-   # img = soup.find("img", {"class":"avatar width-full avatar-before-user-status"}).attrs["src"]
-   # full_name = soup.find("span",{"class":"p-name vcard-fullname d-block overflow-hidden"})
-   # acc_name = soup.find("span",{"class":"p-nickname vcard-username d-block"})
-   # bio = soup.find("div", {"class":"p-note user-profile-bio mb-3"}).find("div").text
-   # location = soup.find("li",{"itemprop":"homeLocation"}).find("span").text
-   # # email = soup.find("li",{"itemprop":"email"}).find("a").text
+   soup = BeautifulSoup(session.get("https://github.com/supig").content, 'html.parser')
+   try:
+      img = soup.find("img", {"class":"avatar width-full avatar-before-user-status"}).attrs["src"]
+   except:
+      img = soup.find("img", {"class":"avatar width-full rounded-2"}).attrs["src"]
+   full_name = soup.find("span",{"class":"p-name vcard-fullname d-block overflow-hidden"}).text
+   acc_name = soup.find("span",{"class":"p-nickname vcard-username d-block"}).text
+   try:
+      bio = soup.find("div", {"class":"p-note user-profile-bio mb-3"}).find("div").text
+   except:
+      bio = "Unknown"
+   location = soup.find("li",{"itemprop":"homeLocation"}).find("span").text
+   print(img)
+   print(full_name)
+   print(acc_name)
+   print(bio)
+   print(location)
+   # email = soup.find("li",{"itemprop":"email"}).find("a").text
    # information à cherche dans la boucle
    # --------------------------------------
    # Repositories:
@@ -111,41 +122,41 @@ try:
    # print(used_lang_star)
    # --------------------------------------
    # Followers
-   params = {
-      "tab": key_search[2]
-   }  
-   #it assumed that by now you are logged so we can now use .get and fetch any page of your choice
-   follower = BeautifulSoup(session.get("https://github.com/supig", params=params).content, 'html.parser')
-   list_followers = follower.findAll("div",{"class":"d-table table-fixed col-12 width-full py-4 border-bottom border-gray-light"})
-   # cherche les infos pour chaque repo:
-   followers = dict()
-   for star_ in list_stars:
-      repository_star.append(star_.find("div",{"class":"d-inline-block mb-1"}).find("a").attrs["href"].lstrip("/"))
-      try:
-         used_lang_star.append(star_.find("span",{"itemprop":"programmingLanguage"}).text.strip())
-      except:
-         used_lang_star.append("Unknown")
-   # ---
-   # Pagination:
-   pa = star.find("div",{"class":"pagination"})
-   # s'il y a un seul page, on n'a pas besoin des codes au-dessous
-   if pa != None:
-      next_ = pa.findAll("a")
-      # print(next_)
-      for n in next_:
-         # print(n.text)
-         # print(n.attrs["href"])
-         if n.text == "Next":
-            star = BeautifulSoup(session.get(n.attrs["href"]).content, 'html.parser')
-            # star = soup_sub.find("div", {"class":"d-lg-flex gutter-lg mt-4"})
-            list_stars = star.findAll("div",{"class":"col-12 d-block width-full py-4 border-bottom"})
-            for star_ in list_stars:
-               repository_star.append(star_.find("div",{"class":"d-inline-block mb-1"}).find("a").attrs["href"].lstrip("/"))
-               try:
-                  used_lang_star.append(star_.find("span",{"itemprop":"programmingLanguage"}).text.strip())
-               except:
-                  used_lang_star.append("Unknown")
-   print(repository_star)
-   print(used_lang_star)
+   # params = {
+   #    "tab": key_search[2]
+   # }  
+   # #it assumed that by now you are logged so we can now use .get and fetch any page of your choice
+   # follower = BeautifulSoup(session.get("https://github.com/supig", params=params).content, 'html.parser')
+   # list_followers = follower.findAll("div",{"class":"d-table table-fixed col-12 width-full py-4 border-bottom border-gray-light"})
+   # # cherche les infos pour chaque repo:
+   # followers = dict()
+   # for star_ in list_stars:
+   #    repository_star.append(star_.find("div",{"class":"d-inline-block mb-1"}).find("a").attrs["href"].lstrip("/"))
+   #    try:
+   #       used_lang_star.append(star_.find("span",{"itemprop":"programmingLanguage"}).text.strip())
+   #    except:
+   #       used_lang_star.append("Unknown")
+   # # ---
+   # # Pagination:
+   # pa = star.find("div",{"class":"pagination"})
+   # # s'il y a un seul page, on n'a pas besoin des codes au-dessous
+   # if pa != None:
+   #    next_ = pa.findAll("a")
+   #    # print(next_)
+   #    for n in next_:
+   #       # print(n.text)
+   #       # print(n.attrs["href"])
+   #       if n.text == "Next":
+   #          star = BeautifulSoup(session.get(n.attrs["href"]).content, 'html.parser')
+   #          # star = soup_sub.find("div", {"class":"d-lg-flex gutter-lg mt-4"})
+   #          list_stars = star.findAll("div",{"class":"col-12 d-block width-full py-4 border-bottom"})
+   #          for star_ in list_stars:
+   #             repository_star.append(star_.find("div",{"class":"d-inline-block mb-1"}).find("a").attrs["href"].lstrip("/"))
+   #             try:
+   #                used_lang_star.append(star_.find("span",{"itemprop":"programmingLanguage"}).text.strip())
+   #             except:
+   #                used_lang_star.append("Unknown")
+   # print(repository_star)
+   # print(used_lang_star)
 except HTTPError as e:
    print(e)
